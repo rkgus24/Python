79513aa4817b3639c5d225f54fb7ae7981dec1e6
가현 <102145605+rkgus24@users.noreply.github.com>
2022-09-28 10:03:17 +0900

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,12 @@
+# import Travel.Thailand
+
+# trip_to = Travel.Thailand.ThailandPackage()
+# trip_to.detail()
+
+# from Travel import Vietnam
+# trip_to = Vietnam.VietnamPackage()
+# trip_to.detail()
+
+from Travel import *
+trip_to = Vietnam.VietnamPackage()
+trip_to.detail()
