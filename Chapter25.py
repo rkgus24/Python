a8678b8078f4d8c173db450b06d7fed4cd3fd908
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:05:11 +0900

    Update Chapter25.py

---
+++ Chapter25.py
@@ -0,0 +1,7 @@
+def print_arithmetic_operation(a, b):
+	print(f"{a} + {b} = {a + b}")
+	print(f"{a} - {b} = {a - b}")
+	print(f"{a} * {b} = {a * b}")
+	print(f"{a} / {b} = {a / b}")
+	
+print_arithmetic_operation(3, 4)
