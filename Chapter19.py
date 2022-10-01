3655ca63ea3ba902434b7700a706baf7b28827c4
가현 <102145605+rkgus24@users.noreply.github.com>
2022-10-01 15:03:20 +0900

    Update Chapter19.py

---
+++ Chapter19.py
@@ -0,0 +1,10 @@
+dic1 = {"apple" : "사과", "bird" : "새"}
+print(dic1)
+
+dic2 = dict(apple = "사과", bird = "새", bug = "벌레")
+print(dic2)
+
+dic3 = {"apple" : "사과", "bird" : "새", "bug" : "벌레"}
+
+del dic3["bug"]
+print(dic3)
